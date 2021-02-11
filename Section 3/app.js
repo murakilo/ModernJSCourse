e2eecc913894064fa79384bf0ab48c4c3e017de0
murakilo <joshuacullen1993@gmail.com>
2021-02-10 18:27:36 -0800

    s3 25 - DOM selectors for multiple elements wip

--- Section 3/app.js
+++ Section 3/app.js
@@ -52,19 +52,19 @@
 // console.log(val);
 
 
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-// 24. DOM Selectors for Single Elements
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-// Methods to pull things from DOM
-// JQuery was used for a long time for this. Vanilla JS has since gotten a lot 
-// easier to work with and JQuery is now mostly bloat.
-// One thing JQuery is good for - quick simple plugins/scripts
-
-// Single vs multiple element selectors
-// Single selectors return the first element that meets requirements
-// Multiple selectors return arrays, HTMLCollections, etc.
-
+// // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// // 24. DOM Selectors for Single Elements
+// // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// // 
+// // Methods to pull things from DOM
+// // JQuery was used for a long time for this. Vanilla JS has since gotten a lot 
+// // easier to work with and JQuery is now mostly bloat.
+// // One thing JQuery is good for - quick simple plugins/scripts
+// // 
+// // Single vs multiple element selectors
+// // Single selectors return the first element that meets requirements
+// // Multiple selectors return arrays, HTMLCollections, etc.
+// 
 // // document.getElementById()
 // console.log(document.getElementById('task-title')); // <h5 id="task-title" class="beans">Tasks</h5>
 // 
@@ -84,20 +84,43 @@
 // taskTitle.innerText = 'My Tasks';
 // taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-// document.querySelector()
-console.log(document.querySelector('#task-title')); // <h5 id="task-title" class="beans">Tasks</h5>
-console.log(document.querySelector('.card-title')); // <span class="card-title">Task List</span>
-console.log(document.querySelector('h5')); // <h5 id="task-title" class="beans">Tasks</h5>
+// // document.querySelector()
+// console.log(document.querySelector('#task-title')); // <h5 id="task-title" class="beans">Tasks</h5>
+// console.log(document.querySelector('.card-title')); // <span class="card-title">Task List</span>
+// console.log(document.querySelector('h5')); // <h5 id="task-title" class="beans">Tasks</h5>
+// 
+// document.querySelector('li').style.color = 'red'; // changes first (not all) li item to red font color
+// document.querySelector('ul li').style.color = 'blue'; // changes first (not all) li item (inside ul) to red font color
+// document.querySelector('li:last-child').style.color = 'yellow'; // changes last li to yellow
+// document.querySelector('li:nth-child(3)').style.color = 'green'; // changes third li to green
+// document.querySelector('li:nth-child(4)').textContent = 'beans'; // changes fourth li to read 'beans'
+// document.querySelector('li:nth-child(odd)').style.background = '#000'; // changes first (odd) li background to black
+// document.querySelector('li:nth-child(even)').style.background = '#D0D0D0'; // changes second (even) li background to grey
+// // use querySelectorAll to grab all odd/even elements
 
-document.querySelector('li').style.color = 'red'; // changes first (not all) li item to red font color
-document.querySelector('ul li').style.color = 'blue'; // changes first (not all) li item (inside ul) to red font color
-document.querySelector('li:last-child').style.color = 'yellow'; // changes last li to yellow
-document.querySelector('li:nth-child(3)').style.color = 'green'; // changes third li to green
-document.querySelector('li:nth-child(4)').textContent = 'beans'; // changes fourth li to read 'beans'
-document.querySelector('li:nth-child(odd)').style.background = '#000'; // changes first (odd) li background to black
-document.querySelector('li:nth-child(even)').style.background = '#D0D0D0'; // changes second (even) li background to grey
-// use querySelectorAll to grab all odd/even elements
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// 25. DOM Selectors for Multiple Elements
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
+// return HTMLCollection or a nodelist - both similar to arrays
+// can be converted into arrays very easily
+
+// document.getElementsByClassName('')
+const items = document.getElementsByClassName('collection-item'); // returns HTMLCollection(5)
+console.log(items);
+console.log(items[0]);
+items[0].style.color = 'red';
+items[3].textContent = 'Hello World';
+
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+console.log(listItems);
+
+// document.getElementsByTagName
+const lis = document.getElementsByTagName('li'); // returns HTMLCollection(5)
+console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello World';
 
 
 
