3157ebe9b8f678cc23c56e4d3bd38cd60aa44bcf
murakilo <joshuacullen1993@gmail.com>
2021-03-03 17:32:27 -0800

    s4 36 - local storage persistence

--- Section 4/app.js
+++ Section 4/app.js
@@ -1,7 +1,7 @@
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 // SECTION 4 - DOM Projects
-// 34. Task List 1 - UI & Add Task Items
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// 34-36 Task List
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 // define UI vars
 const form = document.querySelector('#task-form');
@@ -10,10 +10,10 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// load all event listeners
 loadEventListeners();
-
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // addTask event
   form.addEventListener('submit', addTask);
   // remove task event
@@ -21,35 +21,72 @@ function loadEventListeners() {
   // clear task event
   clearBtn.addEventListener('click', clearTasks);
   // filter tasks event
-  filter.addEventListener('input', filterTasks)
-
-  console.log('Event listeners loaded.');
+  filter.addEventListener('input', filterTasks);
 }
 
-// add task to Tasks list
+/** get all tasks from local storage and generate taskList*/
+function getTasks() {
+  let tasks = JSON.parse(checkTasksCookie());
+  tasks.forEach(function(task) {
+    addListItem(task);
+  });
+}
+
+/** generates li element inside ul.collection for a task 
+ *  @param {string} taskName value of task
+*/
+function addListItem(taskName) {
+  // create li element to place new task in
+  const li = document.createElement('li');
+  // add class
+  li.className = 'collection-item';
+  // create text node and append to li
+  li.textContent = taskName;
+  // create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  // add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append link element to list item
+  li.appendChild(link);
+  // append list item to ul
+  taskList.appendChild(li);
+}
+
+/** check tasks cookie is valid. reset if invalid
+ * @returns localStorage 'tasks' object
+*/
+function checkTasksCookie() {
+  let existingTasks = localStorage.getItem('tasks');
+  try {
+    JSON.parse(existingTasks);
+    if (!existingTasks) {
+      console.log('tasks cookie is null. setting to empty array string.');
+      localStorage.setItem('tasks', '[]');
+    }
+  }
+  catch(error) {
+    console.error(error);
+    console.log('existing tasks cookie is invalid. resetting to empty array.');
+    localStorage.setItem('tasks', '[]');
+  }
+  return localStorage.getItem('tasks');
+}
+
+/** add task to Tasks list 
+ *  @param {Element} e element calling event
+*/
 function addTask(e) {
+  let newTask = taskInput.value;
   // check value is not empty
-  if (checkEmptyTask(taskInput.value)) {
+  if (checkEmpty(newTask)) {
     alert('Task field empty');
   } 
   else {
-    // create li element to place new task in
-    const li = document.createElement('li');
-    // add class
-    li.className = 'collection-item';
-    // create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // create new link element
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content'; // secondary-content justifies element to right
-    // add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // append link element to list item
-    li.appendChild(link);
-    // append list item to ul
-    taskList.appendChild(li);
-
-    console.log(`Added ${taskInput.value} to Task List`);
+    newTask = newTask.trim();
+    addListItem(newTask);
+    // store task in local storage
+    storeTaskInLocal(newTask);
   }
 
   // clear taskInput value & prevent page reload
@@ -57,12 +94,42 @@ function addTask(e) {
   e.preventDefault();
 }
 
+/** add task to local storage 
+ *  @param {string} inTask task value
+ */
+function storeTaskInLocal(inTask) {
+  let tasksArray = JSON.parse(checkTasksCookie());
+  tasksArray.push(inTask); // push inTask into array
+  localStorage.setItem('tasks', JSON.stringify(tasksArray)); // overwrite local tasks with tasksArray
+}
+
+/** remove task from task list and local storage 
+ *  @param {Element} e element calling event */
 function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
+    // remove li element
     e.target.parentElement.parentElement.remove();
+    
+    // remove from local storage
+    let tasks = JSON.parse(checkTasksCookie());
+    const taskDeleteValue = e.target.parentElement.parentElement.textContent;
+    
+    // removes ALL values that match
+    //tasks = tasks.filter(task => task !== taskDeleteValue);
+
+    // removes first value that matches
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i] == taskDeleteValue) {
+        tasks.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 }
 
+/** clears task list and local storage 
+ *  @param {Element} e element calling event */
 function clearTasks(e) {
   // two methods
   //taskList.innerHTML = '';
@@ -72,8 +139,13 @@ function clearTasks(e) {
       taskList.removeChild(taskList.firstChild);
     }
   }
+  // clear tasks from localStorage
+  // localStorage.setItem('tasks', '[]');
+  localStorage.clear();
 }
 
+/** filter task list based on input. updates with every input 
+ * @param {Element} e element calling event */
 function filterTasks(e) {
   const filterText = e.target.value.toLowerCase();
   const allTasks = document.querySelectorAll(".collection-item");
@@ -85,15 +157,14 @@ function filterTasks(e) {
     else {
       task.style.display = 'none';
     }
-  })
-
-  console.log(filterText);
+  });
 }
 
-// check if task field is empty using regex
-function checkEmptyTask(inText) {
+/** check if task field is empty using regex 
+ * @param {string} inText text to check if empty
+ * @returns {Boolean} returns true if field is empty */
+function checkEmpty(inText) {
   inText = inText.replace(/^\s+/, '').replace(/\s+$/, ''); // remove all whitespace from inText
-  console.log(inText);
   if (inText === '') {
     return true;
   }
