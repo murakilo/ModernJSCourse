dfe3a50f0a3f93ce7f754ca60d72fc115f7e0c16
murakilo <joshuacullen1993@gmail.com>
2021-05-05 20:44:18 -0700

    add scoreboard

--- Section 4/Number Guesser/app.js
+++ Section 4/Number Guesser/app.js
@@ -9,9 +9,11 @@
 */
 
 // game values
-let min = 1, max = 10, guessesLeft = 3;
-let winningNum = getRandomNum(min, max);
-
+let guessesLeft = 3,
+  min = getRandomNum(1, 95),
+  max = getRandomNum(min + 5, 100),
+  winningNum = getRandomNum(min, max),
+  wins = 0, losses = 0;
 
 // ui elements
 const game = document.querySelector('#game');
@@ -20,10 +22,28 @@ const maxNum = document.querySelector('.maxNum');
 const guessBtn = document.querySelector('#guessBtn');
 const guessInput = document.querySelector('#guessInput');
 const message = document.querySelector('.message');
+const winSpan = document.querySelector('.winCount');
+const lossSpan = document.querySelector('.loseCount');
+const scoreCont = document.querySelector('.score');
 
 // set number boundaries
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// set score
+if (!localStorage.getItem('numberWangWins')) {
+  localStorage.setItem('numberWangWins', 0);
+  localStorage.setItem('numberWangLosses', 0);
+}
+else {
+  wins = parseInt(localStorage.getItem('numberWangWins'));
+  winSpan.textContent = wins;
+  losses = parseInt(localStorage.getItem('numberWangLosses'));
+  lossSpan.textContent = losses;
+}
+
+console.log(localStorage.getItem('numberWangWins'));
+console.log(localStorage.getItem('numberWangLosses'));
 
 // play again event listener
 game.addEventListener('mousedown', function(e) {
@@ -32,6 +52,16 @@ game.addEventListener('mousedown', function(e) {
   }
 })
 
+// reset game event listener
+scoreCont.addEventListener('click', function() {
+  localStorage.setItem('numberWangWins', 0);
+  localStorage.setItem('numberWangLosses', 0);
+  wins = 0;
+  losses = 0;
+  winSpan.textContent = 0;
+  lossSpan.textContent = 0;
+});
+
 // listen for guess input
 guessBtn.addEventListener('click', function() {  
   let guess = parseInt(guessInput.value);
@@ -39,15 +69,18 @@ guessBtn.addEventListener('click', function() {
   // validate input
   // check not blank, or < min and > max
   if (isNaN(guess) || guess < min || guess > max) {
+    guessInput.value = '';
     setMessage(`invalid input. please enter a number between ${min} and ${max}.`, 'red');
   }
   // win case
   else if (guess === winningNum) {
+    wins = addWin(wins);
     gameOver(true, `${guess} is correct!`);
   } 
   else { // lose case
     guessesLeft -= 1;
-    if (guessesLeft === 0) {      
+    if (guessesLeft === 0) {
+      losses = addLoss(losses);
       gameOver(false, `you lost. the correct answer was ${winningNum}.<br>that's numberwang!`);
     }
     else {
@@ -76,4 +109,19 @@ function getRandomNum(min, max) {
 function setMessage(msg, color) {
   message.innerHTML = msg;
   message.style.color = color;
+}
+
+function addWin(winNum) {
+  winNum += 1;
+  localStorage.setItem('numberWangWins', winNum);
+  winSpan.textContent = winNum;
+  return winNum;
+}
+
+function addLoss(lossNum) {
+  console.log(lossNum);
+  lossNum += 1;
+  localStorage.setItem('numberWangLosses', lossNum);
+  lossSpan.textContent = lossNum;
+  return lossNum;
 }
